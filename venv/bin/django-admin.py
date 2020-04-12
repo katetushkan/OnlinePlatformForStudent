e7a813fd238b3359d8ef1e734a3275bdf8925e31
katetushkan <Katetushkan@icloud.com>
2020-04-12 18:20:02 +0300

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/katerinatushinskaya/Desktop/1untitled/CourseWork/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
